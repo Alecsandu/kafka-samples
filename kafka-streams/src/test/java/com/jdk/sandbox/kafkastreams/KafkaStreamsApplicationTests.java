abca7aea8549d7a1b5599798dc0189717b81a206
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2026-02-28 13:27:54 +0200

    Kafka streams init module

---
+++ kafka-streams/src/test/java/com/jdk/sandbox/kafkastreams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdk.sandbox.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
